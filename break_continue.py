3c83eb4a981abb796c30c8f696d0cbc0e1d6fd6f
Rajat_Panchotia <47939625+rajatpanchotia@users.noreply.github.com>
2021-05-29 22:09:55 +0530

    Add files via upload

---
+++ break_continue.py
@@ -0,0 +1,12 @@
+cars =["ok","ok","ok","ok","ok","ok","ok"]
+for status in cars:
+    if status=="faulty":
+        ##print("stop the production")
+         print("found the faulty car..")
+         continue
+      ##  break
+    print(f"this is a {status}")
+    print(f"shipping new car:")
+    
+
+
