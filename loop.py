5a3e40d74266c9fee8f219a103954c3fdebe3856
Rajat_Panchotia <47939625+rajatpanchotia@users.noreply.github.com>
2021-05-30 11:21:37 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,13 @@
+##Repeating a loop 20 times
+
+##for i in range(20):
+  ##  print(i)
+##...............................
+
+##example :2
+##Looping to use values in a collection
+##Here's an example of a for loop that doesn't just repeat something a number of times:
+
+kid_ages = (3,7,9)
+for age in kid_ages:
+    print(f'i have a {age} year old age kid')
