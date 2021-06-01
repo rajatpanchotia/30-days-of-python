32c68401fa2c9f91e8eefb80fbfbbd8041769a97
Rajat_Panchotia <47939625+rajatpanchotia@users.noreply.github.com>
2021-06-01 22:02:44 +0530

    Create listslicing.py

---
+++ listslicing.py
@@ -0,0 +1,17 @@
+friends = ["Rajat", "naini", "sam", "rodio", "cheeteh"]
+
+print(friends[2:4])
+print(friends[2:])
+print(friends[:4])
+
+print(friends[:])
+
+print(friends[-3:])
+print(friends[:-2])
+print(friends[-3:-1])
+
+# You can slice with tuples and strings as well.
+# More advanced content in our slices blog posts!
+
+# https://blog.tecladocode.com/python-slices/
+# https://blog.tecladocode.com/python-slices-part-2/
